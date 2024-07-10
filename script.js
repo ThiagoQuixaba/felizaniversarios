eba90c6bbbe673e23f2ed145c528c37ac4adf40d
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2024-07-09 23:57:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function verificarSenha() {
+    const senhaDigitada = document.getElementById('senhaInput').value;
+
+    if (senhaDigitada === "A.M.I.Z.A.D.E") {
+        alert('mE odot ometp ama o ogima, e an aitsugna es zaf o oãnir.')
+    } else if (senhaDigitada === "Provérbios 17:17") {
+        alert('Em que anos nos conhecemos?')
+    } else if (senhaDigitada === "2021") {
+    alert('Feliz Aniversário!\nEspero Que Você Tenha Se Divertido. \nObs: Por Mais Que Tenha Dado Trabalho. 🙃')
+    } 
+}
